058fdea62418568a7300da576e91f3a4b3406bca
Indroic <indroic@outlook.com>
2026-05-02 17:10:57 -0400

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@access-control-system/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
---
+++ packages/auth/src/index.ts
@@ -0,0 +1,33 @@
+import { createDb } from "@access-control-system/db";
+import * as schema from "@access-control-system/db/schema/auth";
+import { env } from "@access-control-system/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export function createAuth() {
+  const db = createDb();
+
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "pg",
+
+      schema: schema,
+    }),
+    trustedOrigins: [env.CORS_ORIGIN],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    advanced: {
+      defaultCookieAttributes: {
+        sameSite: "none",
+        secure: true,
+        httpOnly: true,
+      },
+    },
+    plugins: [],
+  });
+}
+
+export const auth = createAuth();
